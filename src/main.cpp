7050867e20ef2d0bfc8f9945ae8250a24db18596
Rodr1goTavares <rodr1gowzxz2@gmail.com>
2024-10-13 16:14:29 -0300

    :construction: Shell and uptime added.

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,22 @@
 #include <sys/utsname.h>
 #include <unistd.h>
 #include <iomanip>
+#include <unordered_map>
 
-#include <fetchInfo.h>
+#include <fetchSystemInfo.cpp>
 
 using namespace FetchInfo;
 
 
+void previewFetch() {
+    std::unordered_map<std::string, std::string> preview;
+    preview["OS"] = getOSInfo();
+    preview["Kernel"] = getKernelInfo();
+    preview["Shell"] = getShellInfo();
+    preview["Uptime"] = getUptimeInfo();
+}
+
 int main() {
-    std::cout << showKernel() << std::endl;
+    previewFetch();
     return 0;
 }
