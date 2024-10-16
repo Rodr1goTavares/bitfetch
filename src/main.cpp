1309e58d18b5746ef023cb6636ed95f9696a138b
rodrigo henrique <rodr1gowzxz2@gmail.com>
2024-10-16 00:44:17 -0300

    :construction: MVP

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,56 @@
 #include <iomanip>
 #include <unordered_map>
 
-#include <fetchSystemInfo.cpp>
+std::string getOSInfo() {
+	std::ifstream file("/etc/os-release");
+	std::string line;
+	std::string distroName = "Unknown";
+	if (!file.is_open()) return distroName;
+	while (std::getline(file, line)) {
+		if (line.find("NAME=") == 0) {
+			distroName = line.substr(5);
+			if (distroName[0] == '"' || distroName[0] == '\'')
+				distroName = distroName.substr(1, distroName.size() - 2);
+			break;
+		}
+	}
+	file.close();
+	return distroName;
+}
 
-using namespace FetchInfo;
+std::string getKernelInfo() {
+	std::string kernelInfo = "Kernel: ";
+	struct utsname buffer;
+	if (uname(&buffer) != 0) return "Error to get Kernel info :(";
+	kernelInfo += buffer.sysname;
+	return kernelInfo;
+}
 
+std::string getShellInfo() {
+	const char *shell = getenv("SHELL");
+	if (shell == nullptr) return "Unknown";
+	return std::string(shell);
+}
 
-void previewFetch() {
-    std::unordered_map<std::string, std::string> preview;
-    preview["OS"] = getOSInfo();
-    preview["Kernel"] = getKernelInfo();
-    preview["Shell"] = getShellInfo();
-    preview["Uptime"] = getUptimeInfo();
+std::string getUptimeInfo() {
+	return "Unknown";
+}
+
+std::unordered_map<std::string, std::string> previewFetch() {
+	std::unordered_map<std::string, std::string> preview;
+	preview["os"] = getOSInfo();
+	preview["kernel"] = getKernelInfo();
+	preview["shell"] = getShellInfo();
+	preview["uptime"] = getUptimeInfo();
+	return preview;
 }
 
 int main() {
-    previewFetch();
-    return 0;
+	std::cout << "-----------------------------------\n" << std::endl;
+	std::cout << "OS: " << previewFetch()["os"] << "\n" << std::endl;
+	std::cout << "Kernel: " << previewFetch()["kernel"] << "\n" << std::endl;
+	std::cout << "Shell: " << previewFetch()["shell"] << "\n" << std::endl;
+	std::cout << "Uptime: " << previewFetch()["uptime"] << "\n" << std::endl;
+	std::cout << "-----------------------------------" << std::endl;
+	return 0;
 }
