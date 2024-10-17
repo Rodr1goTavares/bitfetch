5ac1e5053ba7c5c113a281a528fef9ec8fe18ff9
rodrigo henrique <rodr1gowzxz2@gmail.com>
2024-10-17 13:30:10 -0300

    :construction: Structure updated

---
+++ src/fetchSystemInfo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <sys/utsname.h>
+#include <unistd.h>
+#include <iomanip>
+#include <cstdlib>
+
+
+namespace fetchSystemInfo {
+    void fuck(std::string name) {
+        std::cout << "Fuck you " << name << std::endl;    
+    }
+}
